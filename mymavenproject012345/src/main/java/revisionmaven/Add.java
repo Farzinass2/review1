0435af9d688dc6029fda47d91aa6d06f6053a0fe
hp <hp@DESKTOP-373P18S>
2025-12-15 19:46:32 +0530

    jik

---
+++ mymavenproject012345/src/main/java/revisionmaven/Add.java
@@ -0,0 +1,12 @@
+package revisionmaven;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b=12;
+int c=a+b;
+System.out.println("The sum is"+c);
+	}
+
+}
